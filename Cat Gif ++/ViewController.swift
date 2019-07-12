7c3011a6e24f0c7d42b335686d45ea409b3954e9
KCordilia <k.cordilia@hotmail.com>
2019-07-12 09:42:09 +0200

    Initial Commit

---
+++ Cat Gif ++/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cat Gif ++
+//
+//  Created by Karim Cordilia on 12/07/2019.
+//  Copyright © 2019 Karim Cordilia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
